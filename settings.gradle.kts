74f853ff2db0a1f51e2bea2ca83a525e7b116651
temur.navoi <temur.navoi@yandex.com>
2022-05-04 19:22:00 +0200

    move kotlin plugins into parent gradle build script

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,9 @@
+plugins {
+    kotlin("jvm") version "1.6.20" apply false
+    kotlin("plugin.spring") version "1.6.20" apply false
+    kotlin("kapt") version "1.6.20" apply false
+}
+
 rootProject.name = "paint_contest"
 
 include ("bot")
