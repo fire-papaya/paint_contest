08d1e5b6241ae06dd22ecb463888c3dc402d980f
temur.navoi <temur.navoi@yandex.com>
2023-11-28 23:53:08 +0100

    remove unused functions in main bot

--- bot/src/main/kotlin/uz/warcom/contest/bot/representation/PaintContestBot.kt
+++ bot/src/main/kotlin/uz/warcom/contest/bot/representation/PaintContestBot.kt
@@ -4,19 +4,22 @@ import org.apache.logging.log4j.LogManager
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 import org.telegram.abilitybots.api.bot.AbilityBot
-import org.telegram.abilitybots.api.objects.Ability
-import org.telegram.abilitybots.api.objects.Flag
-import org.telegram.abilitybots.api.objects.Locality
-import org.telegram.abilitybots.api.objects.Privacy
+import org.telegram.abilitybots.api.bot.BaseAbilityBot
+import org.telegram.abilitybots.api.objects.*
 import org.telegram.abilitybots.api.util.AbilityExtension
 import org.telegram.telegrambots.meta.api.objects.PhotoSize
 import org.telegram.telegrambots.meta.api.objects.Update
 import uz.warcom.contest.bot.config.BotConfiguration
 import uz.warcom.contest.bot.exception.BotException
 import uz.warcom.contest.bot.model.ImageToSave
+import uz.warcom.contest.bot.model.enums.ButtonLabel
+import uz.warcom.contest.bot.model.enums.Commands
+import uz.warcom.contest.bot.model.enums.EmojiCmd
 import uz.warcom.contest.bot.model.enums.UserState
 import uz.warcom.contest.bot.service.AdminService
 import uz.warcom.contest.bot.service.PersistenceFacade
+import uz.warcom.contest.persistence.exception.ContestNotFoundException
+import uz.warcom.contest.persistence.exception.UserWithoutCommunityException
 
 
 @Component
@@ -34,61 +37,6 @@ constructor(
 
     fun publicAbilityExtension(): AbilityExtension {
         return PublicAbilityExtension(this, persistenceFacade)
-    }
-
-    private val contestStates = setOf(UserState.PRIMED, UserState.PAINTED)
-
-    fun processImage(): Ability {
-        return Ability.builder()
-            .name(DEFAULT)
-            .flag(Flag.PHOTO.and { userState(it) in contestStates })
-            .locality(Locality.ALL)
-            .privacy(Privacy.PUBLIC)
-            .action {
-                val state = userState(it.user().id)
-
-                try {
-                    val photo = retrieveHighestQuality(it.update().message.photo)
-
-                    val entry = persistenceFacade.postPicture(ImageToSave(
-                        it.user(),
-                        state == UserState.PAINTED,
-                        photo.fileId
-                    ))
-
-                    val message = if (state == UserState.PRIMED) {
-                        updateUserState(it.user().id, UserState.PAINTED)
-                        "Изображение получено. Отправь три изображения покрашенной миниатюры, когда закончишь покрас"
-                    } else {
-                        "Изображение получено: ${entry.images.filter { img -> img.isReady }.size}/3"
-                    }
-
-                    silent.send(message, it.chatId())
-                } catch (e: BotException) {
-                    silent.send(e.message, it.chatId())
-                }
-            }
-            .build()
-    }
-
-    fun retrieveHighestQuality(photos: List<PhotoSize>): PhotoSize {
-        return photos.stream()
-            .max(Comparator.comparing { obj: PhotoSize -> obj.fileSize })
-            .orElseThrow { IllegalStateException("No photos were found in photos stream") }
-    }
-
-    private fun updateUserState (userId: Long, state: UserState) {
-        db.getMap<String, Any>("USER_STATES").entries.forEach{ ent -> println("${ent.key} : ${ent.value}") }
-        val states: MutableMap<String, String> = db.getMap("USER_STATES")
-        states[userId.toString()] = state.toString()
-    }
-
-    private fun userState (userId: Long): UserState {
-        return UserState.valueOf(db.getMap<String, String>("USER_STATES").getOrDefault(userId.toString(), "START"))
-    }
-
-    private fun userState(update: Update): UserState {
-        return this.userState(update.message.from.id)
     }
 
     override fun creatorId(): Long {
