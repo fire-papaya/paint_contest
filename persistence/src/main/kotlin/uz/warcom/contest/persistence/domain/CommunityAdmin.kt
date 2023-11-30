4dcead2281bfe74cc8846d37e6b0e7b60bb645e4
temur.navoi <temur.navoi@yandex.com>
2023-11-30 20:57:56 +0100

    fix community admin table name

--- persistence/src/main/kotlin/uz/warcom/contest/persistence/domain/CommunityAdmin.kt
+++ persistence/src/main/kotlin/uz/warcom/contest/persistence/domain/CommunityAdmin.kt
@@ -3,8 +3,10 @@ package uz.warcom.contest.persistence.domain
 import javax.persistence.Entity
 import javax.persistence.JoinColumn
 import javax.persistence.ManyToOne
+import javax.persistence.Table
 
 @Entity
+@Table(name = "community_admin_user")
 class CommunityAdmin: AbstractIntEntity() {
     @ManyToOne
     @JoinColumn(name = "user_id")
