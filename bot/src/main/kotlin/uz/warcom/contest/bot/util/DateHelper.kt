de4fda92eb616ff087ba59cacf1d429de6aad95a
temur.navoi <temur.navoi@yandex.com>
2023-11-30 20:58:43 +0100

    util function for parsing dates

--- bot/src/main/kotlin/uz/warcom/contest/bot/util/DateHelper.kt
+++ bot/src/main/kotlin/uz/warcom/contest/bot/util/DateHelper.kt
@@ -1,10 +1,12 @@
 package uz.warcom.contest.bot.util
 
+import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 import java.time.temporal.TemporalAccessor
 
 object DateHelper {
     private val defaultFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm")
+    private val contestDateParser = DateTimeFormatter.ofPattern("ddMMyy")
 
     fun format (format: String, temporalAccessor: TemporalAccessor): String {
         return DateTimeFormatter.ofPattern(format).format(temporalAccessor)
@@ -12,5 +14,9 @@ object DateHelper {
 
     fun format (temporalAccessor: TemporalAccessor): String {
         return defaultFormatter.format(temporalAccessor)
+    }
+
+    fun parse (string: String): LocalDate {
+        return LocalDate.parse(string, contestDateParser)
     }
 }
